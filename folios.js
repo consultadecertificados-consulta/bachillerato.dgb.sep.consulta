f522da81ef33fe1f45b3b53300992438e59b8151
consultadecertificados-consulta <consultadecertificados@gmail.com>
2026-08-08 22:58:19 -0600

    Update file description in folios.js
    Updated the description of the folios.js file to reflect that it is an updated database.

--- folios.js
+++ folios.js
@@ -1,6 +1,5 @@
-p9// folios.js - Contenedor externo de certificados SEMS
+// folios.js - Base de datos actualizada
 const baseDatos = {
-
      "CBG376039125500": { nombre: "RICARDO MARTINEZ CASANOVA", matricula: "1701M960101", emisor: "DIRECCIÃ“N GENERAL DE BACHILLERATO ABIERTO DE LA CIUDAD DE MÃ‰XICO", plantel: "IZTAPALAPA CCT-09GBH0007V", plan: "BACHILLERATO INTENSIVO GENERAL", promedio: "8.5", periodo: "21-07-2021 al 15-08-2022", estatus: "REGISTRADO", timbrado: "2022-08-25 16:20:11" },
     "CBG376039125100": { nombre: "ISRAEL INDA DELGADO", matricula: "1702R950202", emisor: "DIRECCION GENERAL DE BACHILLERATO DEL ESTADO DE AGUASCALIENTES", plantel: "AGUASCALIENTES CCT-01EEX0001Z", plan: "BACHILLERATO INTENSIVO GENERAL", promedio: "8.5", periodo: "21-07-2016 al 15-08-2018", estatus: "REGISTRADO", timbrado: "2018-08-25 16:20:11" },
     "CBG376039125200": { nombre: "EDUARDO LOPEZ ZAMORA", matricula: "1703M940303", emisor: "DIRECCION GENERAL DE BACHILLERATO DEL ESTADO DE MÉXICO", plantel: "ATLAUTLA CCT-15DEX0001C", plan: "BACHILLERATO INTENSIVO GENERAL", promedio: "8.5", periodo: "21-07-2023 al 15-08-2025", estatus: "REGISTRADO", timbrado: "2025-08-25 16:20:11" },
